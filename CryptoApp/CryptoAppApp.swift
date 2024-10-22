45a84b9d0dd126d16df85972fe385cbe569d021c
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-10-22 22:04:53 +0530

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Jiten Patel on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
